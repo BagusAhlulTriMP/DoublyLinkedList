3e383abfaeb748502c970e3208e5e5712de455be
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2022-11-16 17:24:35 +0700

    membuat method search

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -75,5 +75,16 @@ namespace DoublyLinkedList
             previous.next = newnode;
         }
 
+        /*Checks wheteher the specified node is present*/
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = START; current != null &&
+                rollNo != current.rollNumber; previous = current, current = current.next)
+            { }
+            /*The above for loop traverses the list. if the specified node
+             * is found then the function returns true, otherwise false. */
+            return (current == null);
+        }
+
     }
 }
